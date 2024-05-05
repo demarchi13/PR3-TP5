bb1422db99e71a3ee712bb80a8fb79748b6b358d
Lucas De Marchi <131633835+demarchi13@users.noreply.github.com>
2024-05-05 14:57:37 -0300

    Update AgregarSucursal.aspx.cs

--- TP5_GRUPO_NRO_10/AgregarSucursal.aspx.cs
+++ TP5_GRUPO_NRO_10/AgregarSucursal.aspx.cs
@@ -10,30 +10,35 @@ namespace TP5_GRUPO_NRO_10
 {
     public partial class AgregarSucursal : System.Web.UI.Page
     {
+        AccesoDatos conexion = new AccesoDatos(); //PUNTO 7
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
+                //PUNTO 5
                 ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
-                AccesoDatos conexion = new AccesoDatos();
+                
+                //PUNTO 7
                 conexion.CargarDdlProvincia(ddlProvincia);
             }
         }
 
         protected void btnAceptar_Click(object sender, EventArgs e)
         {
+            //PUNTO 8
             AccesoDatos conexion = new AccesoDatos();
-
             conexion.setearConsulta("INSERT INTO Sucursal (NombreSucursal, DescripcionSucursal, Id_ProvinciaSucursal, DireccionSucursal) VALUES ('" + txtNombreSucursal.Text + "','" + txtDescripcion.Text + "'," + ddlProvincia.SelectedValue + ",'" + txtDireccion.Text + "')");
 
+            //PUNTO 9
             if (conexion.ejecutarAccion())  lblMensaje.Text = "La sucursal se ha agregado con exito";
             else lblMensaje.Text = "La sucursal no se pudo cargar";
-            
-            conexion.cerrarConexion();
 
+            //PUNTO 11
+            conexion.cerrarConexion();
             LimpiarCampos();
         }
 
+        // PUNTO 10
         public void LimpiarCampos()
         {
             txtNombreSucursal.Text = "";
