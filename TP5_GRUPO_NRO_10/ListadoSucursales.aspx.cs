17b08a3d10ff2fd7308a3eeebdb73c472883e2bf
Lucas De Marchi <131633835+demarchi13@users.noreply.github.com>
2024-05-05 15:00:44 -0300

    Update ListadoSucursales.aspx.cs

--- TP5_GRUPO_NRO_10/ListadoSucursales.aspx.cs
+++ TP5_GRUPO_NRO_10/ListadoSucursales.aspx.cs
@@ -10,12 +10,15 @@ namespace TP5_GRUPO_NRO_10
 {
     public partial class ListadoSucursales : System.Web.UI.Page
     {
+        //PUNTO 5
         AccesoDatos conexion = new AccesoDatos();
         protected void Page_Load(object sender, EventArgs e)
-        {
+        {            
             if (!IsPostBack)
             {
+                //PUNTO 6
                 ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+                //PUNTO 9
                 conexion.CargarGrillaSucursales(gvSucursales);
 
             }
@@ -23,11 +26,13 @@ namespace TP5_GRUPO_NRO_10
 
         protected void btnMostrarTodos_Click(object sender, EventArgs e)
         {        
+            //PUNTO 10
             conexion.CargarGrillaSucursales(gvSucursales);
         }
 
         protected void btnFiltrar_Click(object sender, EventArgs e)
         {
+            //PUNTO 11
             conexion.FiltrarGrilla(gvSucursales, txtIdSucursal);
         }
     }
