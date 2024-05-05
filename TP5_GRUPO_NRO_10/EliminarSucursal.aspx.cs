c5c6baaf7577f7a983c8bf2bd3434771f9d9b46a
Lucas De Marchi <131633835+demarchi13@users.noreply.github.com>
2024-05-05 15:02:27 -0300

    Update EliminarSucursal.aspx.cs

--- TP5_GRUPO_NRO_10/EliminarSucursal.aspx.cs
+++ TP5_GRUPO_NRO_10/EliminarSucursal.aspx.cs
@@ -9,17 +9,20 @@ namespace TP5_GRUPO_NRO_10
 {
     public partial class EliminarSucursal : System.Web.UI.Page
     {
+        //PUNTO 9
         AccesoDatos conexion = new AccesoDatos();
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
+                //PUNTO 10
                 ValidationSettings.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
             }
         }
 
         protected void btnEliminar_Click(object sender, EventArgs e)
         {
+            //PUNTO 11
             conexion.EliminarSucursal(txtID, lblMensaje);
         }
     }
